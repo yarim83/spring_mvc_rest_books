8c6aca7495b08e9fa53d998a18e9f7f7cb17084f
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-12-21 14:58:35 +0100

    Zadanie 7

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,21 +1,42 @@
 package pl.coderslab.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "{hello: World}";
     }
 
     @RequestMapping("/helloBook")
-    public Book helloBook(){
-        return new Book(1L,"9788324631766","Thinking in Java",
-                "Bruce Eckel","Helion","programming");    }
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+
+    private MemoryBookService memoryBookService;
+
+    public BookController(MemoryBookService memoryBookService){
+        this.memoryBookService = memoryBookService;
+    }
+
+    @RequestMapping("/")
+    public List<Book> allBooks(MemoryBookService memoryBookService){
+        return memoryBookService.getList();
+    }
+
+    @RequestMapping("/{id}")
+    public Book book(@PathVariable long id){
+        return memoryBookService.getBook(id);
+    }
+
 }
---
+++ src/main/java/pl/coderslab/controller/MemoryBookService.java
@@ -0,0 +1,41 @@
+package pl.coderslab.controller;
+
+
+import org.springframework.stereotype.Service;
+import org.w3c.dom.ls.LSException;
+import pl.coderslab.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MemoryBookService {
+    private List<Book> list;
+
+    public MemoryBookService() {
+        list = new ArrayList<>();
+        list.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel",
+                "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz glowa, Java.",
+                "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        list.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
+    }
+
+    public List<Book> getList() {
+        return list;
+    }
+
+    public void setList(List<Book> list) {
+        this.list = list;
+    }
+
+    public Book getBook(long id){
+        for (Book book: list){
+            if (book.getId() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
