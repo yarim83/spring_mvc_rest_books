72c752cd99fd2ff2e6cd040eab3d6f15b088b333
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-12-21 14:27:52 +0100

    Testowa inicjalizacja README.md

---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,21 @@
+package pl.coderslab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.model.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "{hello: World}";
+    }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");    }
+}
