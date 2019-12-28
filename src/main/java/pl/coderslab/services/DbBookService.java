3d6e4f90924f923f3698f0d9147bb02b02ee7600
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-12-28 12:31:02 +0100

    Zadania rozszeżone

--- src/main/java/pl/coderslab/services/DbBookService.java
+++ src/main/java/pl/coderslab/services/DbBookService.java
@@ -1,7 +1,44 @@
 package pl.coderslab.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.coderslab.dao.BookDao;
+import pl.coderslab.model.Book;
+
+import java.util.List;
 
 @Service
-public class DbBookService {
+public class DbBookService implements BookService {
+
+    private final BookDao bookDao;
+
+    @Autowired
+    public DbBookService(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Override
+    public List<Book> getList() {
+        return null;
+    }
+
+    @Override
+    public void create(Book book) {
+        bookDao.create(book);
+    }
+
+    @Override
+    public Book getBookById(long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Book book) {
+
+    }
+
+    @Override
+    public void deleteBookById(long id) {
+
+    }
 }
