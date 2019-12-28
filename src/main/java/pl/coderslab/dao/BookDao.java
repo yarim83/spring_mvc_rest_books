113120e59f1f0e2c904080343019b29d34d4ed2a
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-12-28 11:58:49 +0100

    Zadania rozszeżone

---
+++ src/main/java/pl/coderslab/dao/BookDao.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dao;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public class BookDao  {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+}
